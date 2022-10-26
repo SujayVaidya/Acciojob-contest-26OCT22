58df1008ce502e7d77af34c290bd5494fd7b5417
SujayVaidya <sujayvaidya4@gmail.com>
2022-10-27 00:02:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import MainView from './Components/Layout/MainView';
+import RenderApi from './Components/Render/RenderApi';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h3>Products</h3>
+     
+     <RenderApi/>
+     {/* <MainView/> */}
+    </>
   );
 }
 
---
+++ src/Components/Render/RenderApi.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './RenderApi.css';
+import { useEffect , useState } from 'react';
+import MainView from '../Layout/MainView';
+
+
+
+const RenderApi = () => {
+    const [productList, setProductList] = useState([]);
+
+    const getProductInfo = async () =>{
+        await fetch("https://acciojob-module-7-sept-2022-default-rtdb.asia-southeast1.firebasedatabase.app/products.json").then((response) => {return response.json()}).then((data)=>{
+            const productInfo =[];
+            for(const id in data){
+                const product = {
+                    id : id,
+                    ...data[id]
+                }
+                productInfo.push(product);
+            }
+            setProductList(productInfo);
+        })
+        
+        
+        
+        
+    };
+    //const data = await response.json();
+ useEffect(() => {
+    
+    getProductInfo();
+    
+    
+ }, []);
+
+ 
+    return (
+          <>  
+{
+               productList.map((items)=>{
+                return(
+                    <ul key={items.id} >
+                    <li ><MainView  
+                                  productImage = {items.productImage}
+                                  productName = {items.productName}
+                                  id = {items.id}
+                                  oldPrice = {items.oldPrice}
+                                  newPrice = {items.newPrice}
+                                     />
+                                     </li>
+                                     </ul>
+                );
+                
+               }
+               )
+
+                
+}
+
+     </>       
+  )
+}
+
+export default RenderApi
